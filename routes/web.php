df7ca53dd0c7b64800e4eabaaac84a1f73e337e7
vagrant <vagrant@homestead>
2018-09-13 03:57:27 +0000

    商品详情页

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Auth::routes();
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
